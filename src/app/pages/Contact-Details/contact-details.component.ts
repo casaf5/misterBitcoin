3469a5fbe3f4b4ffa2bf0da13b027150f24cd8b9
Asaf Cohen <casaf5@gmail.com>
2020-08-12 09:50:25 +0300

    second upload

--- src/app/pages/Contact-Details/contact-details.component.ts
+++ src/app/pages/Contact-Details/contact-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ContactService } from 'src/app/services/ContactService/contact.service';
+import { UserService } from 'src/app/services/UserService/user.service';
+import { Move } from 'src/app/interfaces/move.interface';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'contact-details',
@@ -12,7 +15,8 @@ export class ContactDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private contactService: ContactService
+    private contactService: ContactService,
+    private userService: UserService
   ) {}
 
   contactToShow;
@@ -22,10 +26,14 @@ export class ContactDetailsComponent implements OnInit {
       .getContactById(id)
       .subscribe((c) => (this.contactToShow = c));
   }
-  goBack():void{
-    this.location.back()
+  goBack(): void {
+    this.location.back();
+  }
+  get userTransactions(): any[] {
+    const user :User= this.userService.loggedUser;
+    return user.moves.filter((move:Move) => move.to_name === this.contactToShow.name);
   }
   ngOnInit(): void {
-    this.getContact()
+    this.getContact();
   }
 }
